7da17d20395bb7fd791b51cc405171a5f140715d
ilyavladimirov1 <112639185+ilyavladimirov1@users.noreply.github.com>
2024-03-31 18:30:47 +0300

    1 задание

--- server.py
+++ server.py
@@ -4,21 +4,17 @@ sock = socket.socket()
 sock.bind(('', 9090))
 print("Server starting!")
 sock.listen(0)
-print("Port is listing")
+print("Port is listening")
 conn, addr = sock.accept()
 print("Client accepted")
-print("Client adress: ", addr[0])
+print("Client address:", addr[0])
 print("Client port:", addr[1])
 
-msg = ''
 
 while True:
 	data = conn.recv(1024)
-	if not data:
-		break
-	msg += data.decode()
-	conn.send(msg.upper().encode())
-
-print(msg)
+    	if data.decode().strip() == 'exit':
+        	break
+    	conn.send(data.upper())
 
 conn.close()
