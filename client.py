e36d9e8a26db135cfd60d108319215c9463b7667
ilyavladimirov1 <112639185+ilyavladimirov1@users.noreply.github.com>
2024-03-31 18:32:04 +0300

    4 задание
    Запрашиваю у пользователя номера порта и имя хоста (для клиента)

--- client.py
+++ client.py
@@ -1,15 +1,15 @@
 import socket
 
-sock = socket.socket()
-sock.setblocking(1)
-sock.connect(('localhost', 9090))
+host = input("Enter server hostname or IP address: ")
+port = int(input("Enter server port: "))
 
-while True:
-    msg = input("Your string (type 'exit' to quit): ")
-    sock.send(msg.encode())
-    if msg.strip() == 'exit':
-        break
-    data = sock.recv(1024)
-    print(data.decode())
+sock = socket.socket()
+sock.connect((host, port))
+
+msg = input("Enter your message: ")
+sock.send(msg.encode())
+
+data = sock.recv(1024)
+print(data.decode())
 
 sock.close()
